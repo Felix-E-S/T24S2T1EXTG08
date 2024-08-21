324519ac5d17f563d3fb09278cecae56228d9bd9
Felix Stelzner <Felix.Stelzner@student.tafesa.edu.au>
2024-08-21 21:12:06 +0930

    Implemented the Main Menu as well as the initial pages MortgageCalc.xaml and CurrencyConv.xaml as well as the according xaml.cs files. all are assessable through the Menu.xaml

---
+++ UniversalCalculator/Menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class Menu : Page
+	{
+		public Menu()
+		{
+			this.InitializeComponent();
+		}
+
+		private void mathsCalculator_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MainPage));
+		}
+		private void mortgageCalculator_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MortgageCalc));
+		}
+		private void currencyConverter_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(CurrencyConv));
+		}
+		private void exit_Click(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+
+	}
+}
